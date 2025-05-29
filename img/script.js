0657222c5fcd6b03c4e7c0a28df6a8e1c6e386cb
pimentelamanda <164514050+pimentelamanda@users.noreply.github.com>
2025-05-29 10:48:01 -0300

    123

---
+++ img/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded') , function(){
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const botaoDeAcessibilidade = document.getElementById('0pcao-acessibilidade')
+
+    botaoDeAcessibilidade.addEventListener('click', acessibilidade) {
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        botaoDeAcessibilidade.classList.toggle('apresenta-lista')
+    }
+}
